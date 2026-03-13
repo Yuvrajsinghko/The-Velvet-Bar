591bbafeab744fdc9ce3b0239d812a122054c951
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-13 10:32:42 +0530

    implemented contact section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -63,7 +63,7 @@ const Hero = () => {
   return (
     <>
       <section id="hero" className="noisy">
-        <h1 className="title">MOJITO</h1>
+        <h1 className="title uppercase">Sofrito</h1>
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
